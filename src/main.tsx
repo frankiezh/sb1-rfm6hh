f9a81bcccbdf723e098a0aa18282677d794099cd
Frank Eisenlohr <frank.eisenlohr@doclift.com>
2024-12-10 08:35:09 +0100

    chore: update deployment settings to prevent indexing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
   <HelmetProvider>
+    <Helmet>
+      <meta name="robots" content="noindex,nofollow" />
+      <meta name="googlebot" content="noindex,nofollow" />
+    </Helmet>
     <App />
   </HelmetProvider>
 );
